48331d7c548986f72cdff8c6677f49390a72d263
Mardanov Timur Rustemovich <timurmardanov97@gmail.com>
2018-09-24 18:21:49 +0300

    Fixes

--- neomodel/core.py
+++ neomodel/core.py
@@ -526,8 +526,10 @@ class StructuredNode(NodeBase, UUID):
             if hasattr(self, 'updated_at') and self.updated_at is not None:
                 if isinstance(self.updated_at, datetime.datetime):
                     self.__properties__['updated_at'] = datetime.datetime.now()
+                    self.updated_at = datetime.datetime.now()
                 elif isinstance(self.updated_at, datetime.date):
                     self.__properties__['updated_at'] = datetime.date.today()
+                    self.updated_at = datetime.date.today()
             params = self.deflate(self.__properties__, self)
             query = "MATCH (n) WHERE id(n)={self} \n"
             query += "\n".join(["SET n.{} = {{{}}}".format(key, key) + "\n"
