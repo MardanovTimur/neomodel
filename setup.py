7dbd607b606ce3ef981ae7a193edf84bcb68f92b
Timur Mardanov <timurmardanov97@gmail.com>
2020-08-11 03:01:06 +0300

    release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='neomodel-next',
-    version='3.5.2.9',
+    version='3.5.3.0',
     description='An object mapper for the neo4j graph database.',
     long_description=open('README.rst').read(),
     author='Robin Edwards; Mardanov Timur',
